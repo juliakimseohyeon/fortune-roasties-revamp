e80cc05d2c43aa8b22f00edda174689335303057
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-10 18:13:34 -0400

    Create useEffect which would gather all fortunes on initial load, which will be necessary for completing the generateRandNum function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.css";
 import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
 import FortuneCookieClosed from "./pages/FortuneCookieClosed/FortuneCookieClosed";
 import FortuneCookieOpen from "./pages/FortuneCookieOpen/FortuneCookieOpen";
 
 function App() {
-  // const generateRandNum = Math.floor(Math.random() * fortunes.length); // Generate a random number between 0 and index number of fortunes
-  const generateRandNum = Math.floor(Math.random() * 10);
+  const [fortunes, setFortunes] = useState([]);
+
+  const generateRandNum = Math.floor(Math.random() * fortunes.length); // Generate a random number between 0 and index number of fortunes
+
+  useEffect(() => {
+    const getAllFortunes = async () => {
+      try {
+        const response = await axios.get(`/fortune`);
+        setFortunes(response);
+      } catch (err) {
+        console.error("Error getting all fortunes: ", err);
+      }
+    };
+    getAllFortunes;
+  }, []);
 
   return (
     <BrowserRouter>
