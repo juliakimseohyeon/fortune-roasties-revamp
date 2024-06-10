426681fbc40d9faf4acc1f142760347365a4baab
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-10 18:02:17 -0400

    Set up basic code structure for FortuneCookieClosed

--- src/pages/FortuneCookieClosed/FortuneCookieClosed.jsx
+++ src/pages/FortuneCookieClosed/FortuneCookieClosed.jsx
@@ -1,5 +1,14 @@
 import "./FortuneCookieClosed";
+import closedFortuneCookie from "../../assets/images/FortuneCookie.svg";
+import { useNavigate } from "react-router-dom";
 
 export default function FortuneCookieClosed() {
-  return;
+  return (
+    <section className="container">
+      <h1>Fortune Roasties</h1>
+      <h2>Open your fortune, if you dare...</h2>
+      <img src={closedFortuneCookie} alt={"closed fortune cookie"} />
+      <button onClick={handleClickGetFortune}>Read Your Fortune</button>
+    </section>
+  );
 }
