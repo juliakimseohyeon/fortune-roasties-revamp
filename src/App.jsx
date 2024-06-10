43e84ddb587fe2c268608fe2b6e684938394a176
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-10 18:05:57 -0400

    Create function to randomly generate number

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import FortuneCookieClosed from "./pages/FortuneCookieClosed/FortuneCookieClosed
 import FortuneCookieOpen from "./pages/FortuneCookieOpen/FortuneCookieOpen";
 
 function App() {
+  // const generateRandNum = Math.floor(Math.random() * fortunes.length); // Generate a random number between 0 and index number of fortunes
+  const generateRandNum = Math.floor(Math.random() * 10);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<FortuneCookieClosed />} />
-        <Route path="/:id" element={<FortuneCookieOpen />} />
+        <Route
+          path="/"
+          element={<FortuneCookieClosed generateRandNum={generateRandNum} />}
+        />
+        <Route
+          path="/:id"
+          element={<FortuneCookieOpen generateRandNum={generateRandNum} />}
+        />
       </Routes>
     </BrowserRouter>
   );
