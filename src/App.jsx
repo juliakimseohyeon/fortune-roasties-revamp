c2aa59432c59fd99f2e4078c8c19421304d9c6e9
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-10 18:00:46 -0400

    Set up routes for fortune cookie pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import FortuneCookieClosed from "./pages/FortuneCookieClosed/FortuneCookieClosed";
+import FortuneCookieOpen from "./pages/FortuneCookieOpen/FortuneCookieOpen";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FortuneCookieClosed />} />
+        <Route path="/:id" element={<FortuneCookieOpen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
