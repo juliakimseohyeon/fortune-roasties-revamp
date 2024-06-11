0e3b31558c2b14f506c50a755036e5397aba126e
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-11 18:13:29 -0400

    Add styles and animation for toggle button

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -31,11 +31,11 @@ export default function Toggle() {
       <input
         type="checkbox"
         id="toggle"
-        className="toggle--checkbox"
+        className="toggle__checkbox"
         onClick={handleOnClick}
       />
-      <label htmlFor="toggle" className="toggle--label">
-        <span className="toggle--label-background"></span>
+      <label htmlFor="toggle" className="toggle__label">
+        <span className="toggle__label-background"></span>
       </label>
     </div>
   );
