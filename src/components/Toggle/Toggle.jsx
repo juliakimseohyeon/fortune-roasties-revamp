e821bd578a4d69a270e0bc29aa143f28994c6fdb
Julia Kim <juliakimseohyeon@gmail.com>
2024-06-11 15:27:36 -0400

    Create onClick function for Toggle component to toggle between light and dark theme

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,14 +1,39 @@
 import "./Toggle.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { setTheme } from "../../utils/themes";
 
 export default function Toggle() {
   const [toggle, setToggle] = useState("dark");
   let theme = localStorage.getItem("theme");
 
+  // OnClick to change theme and toggle
+  const handleOnClick = () => {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setTheme("theme-light");
+      setToggle("light");
+    } else {
+      setTheme("theme-dark");
+      setToggle("dark");
+    }
+  };
+
+  // Ensures the local state toggle always loads with the correct theme
+  useEffect(() => {
+    if (localStorage.getItem("theme") === "theme-dark") {
+      setToggle("dark");
+    } else if (localStorage.getItem("theme") === "theme-light") {
+      setToggle("light");
+    }
+  }, [theme]);
+
   return (
     <div className="container--toggle">
-      <input type="checkbox" id="toggle" className="toggle--checkbox" />
+      <input
+        type="checkbox"
+        id="toggle"
+        className="toggle--checkbox"
+        onClick={handleOnClick}
+      />
       <label htmlFor="toggle" className="toggle--label">
         <span className="toggle--label-background"></span>
       </label>
